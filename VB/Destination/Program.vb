50b5649cc7cc4353ae0b68e57097812a336a37f8
DevExpressExampleBot <fakeEmail>
2022-02-02 12:10:46 +0300

    Source auto update [skip ci]

--- VB/Destination/Program.vb
+++ VB/Destination/Program.vb
@@ -1,47 +1,40 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports DevExpress.Pdf
 
 Namespace Destination
+
     Friend Class Program
 
-        Shared Sub Main(ByVal args() As String)
+        Shared Sub Main(ByVal args As String())
             ' Create a PDF document processor.
-            Using documentProcessor As New PdfDocumentProcessor()
-
+            Using documentProcessor As DevExpress.Pdf.PdfDocumentProcessor = New DevExpress.Pdf.PdfDocumentProcessor()
                 ' Define search words.
-                Dim words() As String = { "DX-B5000", "DX-RX800" }
-
+                Dim words As String() = {"DX-B5000", "DX-RX800"}
                 ' Load a PDF document. 
                 documentProcessor.LoadDocument("..\..\Document.pdf")
-
                 ' Specify the search parameters.
-                Dim searchParameters As New PdfTextSearchParameters()
+                Dim searchParameters As DevExpress.Pdf.PdfTextSearchParameters = New DevExpress.Pdf.PdfTextSearchParameters()
                 searchParameters.CaseSensitive = True
                 searchParameters.WholeWords = True
-
                 ' Get bookmark list. 
-                Dim bookmarks As IList(Of PdfBookmark) = documentProcessor.Document.Bookmarks
+                Dim bookmarks As System.Collections.Generic.IList(Of DevExpress.Pdf.PdfBookmark) = documentProcessor.Document.Bookmarks
                 For Each word As String In words
                     ' Get the search results from the FindText method called with search text and search parameters.
-                    Dim results As PdfTextSearchResults = documentProcessor.FindText(word, searchParameters)
-
+                    Dim results As DevExpress.Pdf.PdfTextSearchResults = documentProcessor.FindText(word, searchParameters)
                     ' If the desired text is found, create a destination that corresponds to the found text
                     ' to be displayed at the upper corner of the window.
-                    If results.Status = PdfTextSearchStatus.Found Then
-                        Dim destination As New PdfXYZDestination(results.Page, 0, results.Rectangles(0).Top, Nothing)
-
+                    If results.Status = DevExpress.Pdf.PdfTextSearchStatus.Found Then
+                        Dim destination As DevExpress.Pdf.PdfXYZDestination = New DevExpress.Pdf.PdfXYZDestination(results.Page, 0, results.Rectangles(CInt((0))).Top, Nothing)
                         ' Create a bookmark with the search word shown as title and the destination.      
-                        Dim bookmark As New PdfBookmark() With {.Title = word, .Destination = destination}
-
+                        Dim bookmark As DevExpress.Pdf.PdfBookmark = New DevExpress.Pdf.PdfBookmark() With {.Title = word, .Destination = destination}
                         ' Add the bookmark to the bookmark list.
                         bookmarks.Add(bookmark)
                     End If
-                Next word
+                Next
+
                 ' Save the modified document.
                 documentProcessor.SaveDocument("..\..\Result.pdf")
             End Using
         End Sub
     End Class
 End Namespace
-
-
