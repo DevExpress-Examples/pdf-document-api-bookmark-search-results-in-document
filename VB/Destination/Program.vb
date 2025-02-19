9fb5a741fb89e34d2939d2cbfbec701c38d80abc
DevExpressExampleBot <fakeEmail>
2025-02-19 18:35:24 +0400

    autogenerate VB

--- VB/Destination/Program.vb
+++ VB/Destination/Program.vb
@@ -1,4 +1,5 @@
 Imports System.Collections.Generic
+Imports System.Diagnostics
 Imports DevExpress.Pdf
 
 Namespace Destination
@@ -17,14 +18,14 @@ Namespace Destination
                 searchParameters.CaseSensitive = True
                 searchParameters.WholeWords = True
                 ' Get bookmark list. 
-                Dim bookmarks As System.Collections.Generic.IList(Of DevExpress.Pdf.PdfBookmark) = documentProcessor.Document.Bookmarks
+                Dim bookmarks As IList(Of DevExpress.Pdf.PdfBookmark) = documentProcessor.Document.Bookmarks
                 For Each word As String In words
                     ' Get the search results from the FindText method called with search text and search parameters.
                     Dim results As DevExpress.Pdf.PdfTextSearchResults = documentProcessor.FindText(word, searchParameters)
                     ' If the desired text is found, create a destination that corresponds to the found text
                     ' to be displayed at the upper corner of the window.
-                    If results.Status = DevExpress.Pdf.PdfTextSearchStatus.Found Then
-                        Dim destination As DevExpress.Pdf.PdfXYZDestination = New DevExpress.Pdf.PdfXYZDestination(results.Page, 0, results.Rectangles(CInt((0))).Top, Nothing)
+                    If results.Status Is DevExpress.Pdf.PdfTextSearchStatus.Found Then
+                        Dim destination As DevExpress.Pdf.PdfXYZDestination = New DevExpress.Pdf.PdfXYZDestination(results.Page, 0, results.Rectangles(0).Top, Nothing)
                         ' Create a bookmark with the search word shown as title and the destination.      
                         Dim bookmark As DevExpress.Pdf.PdfBookmark = New DevExpress.Pdf.PdfBookmark() With {.Title = word, .Destination = destination}
                         ' Add the bookmark to the bookmark list.
@@ -35,6 +36,8 @@ Namespace Destination
                 ' Save the modified document.
                 documentProcessor.SaveDocument("..\..\..\Result.pdf")
             End Using
+
+            Process.Start(New ProcessStartInfo("..\..\..\Result.pdf") With {.UseShellExecute = True})
         End Sub
     End Class
 End Namespace
